df8c5731c764773a52a1985db78f741de46640cf
John Wregglesworth <wregglej@cyverse.org>
2019-01-24 08:56:26 -0700

    Add ErrorSnackbarContent, add error icon, turn error snackbar red

--- client-landing/src/components/ErrorSnackbar.js
+++ client-landing/src/components/ErrorSnackbar.js
@@ -6,12 +6,72 @@ import { setHttpCode } from '../actions';
 
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import ErrorIcon from '@material-ui/icons/Error';
 
-const styles = theme => ({
+import red from '@material-ui/core/colors/red';
+
+const contentStyles = theme => ({
   close: {
     padding: theme.spacing.unit / 2,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  error: {
+    backgroundColor: red[600],
+  },
+  icon: {
+    fontSize: 20,
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+});
+
+function ErrorSnackbarContentUnstyled(props) {
+  const { classes, message, onClose, ...other } = props;
+
+  return (
+    <SnackbarContent
+      className={classes.error}
+      aria-describedby="error-snackbar"
+      message={
+        <span id="error-snackbar" className={classes.message}>
+          <ErrorIcon className={classes.icon} />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          className={classes.close}
+          onClick={onClose}
+        >
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+      {...other}
+    />
+  );
+}
+
+ErrorSnackbarContentUnstyled.propTypes = {
+  classes:   PropTypes.object.isRequired,
+  className: PropTypes.string,
+  message:   PropTypes.node,
+  onClose:   PropTypes.func,
+};
+
+const ErrorSnackbarContent = withStyles(contentStyles)(ErrorSnackbarContentUnstyled);
+
+const snackbarStyles = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
   },
 });
 
@@ -24,7 +84,7 @@ class ErrorSnackbar extends Component {
             errorContent = "Closing..."
             break;
         case 403:
-            errorContent = "Unauthorized access. Please login.";
+            errorContent = "Unauthorized access. asdfasdf Please login.";
             break;
         case 500:
             errorContent = "An error occurred. Please try again.";
@@ -45,23 +105,20 @@ class ErrorSnackbar extends Component {
           ContentProps={{
             'aria-describedby': 'message-id',
           }}
-          message={<span id="message-id">{errorContent}</span>}
-          action={[
-            <IconButton
-              key="close"
-              aria-label="Close"
-              color="inherit"
-              className={classes.close}
-              onClick={handleClose}
-            >
-              <CloseIcon />
-            </IconButton>,
-          ]}
-        />
+        >
+          <ErrorSnackbarContent
+            message={errorContent}
+            onClose={handleClose}
+          />
+        </Snackbar>
       </div>
     );
   }
 }
+
+ErrorSnackbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = state => ({
     httpCode: state.httpCode,
@@ -77,6 +134,6 @@ const MappedErrorSnackbar = connect(
 )(ErrorSnackbar);
 
 export default withStyles(
-    styles,
+    snackbarStyles,
     {withTheme: true}
 )(MappedErrorSnackbar);
